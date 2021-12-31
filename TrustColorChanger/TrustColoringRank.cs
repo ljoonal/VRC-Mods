26f1b9b547fa2a3132831750f282a1dfc6bde4f1
ljoonal <ljoonal@ljoonal.xyz>
2021-12-31 18:50:27 +0200

    Cleanup switch expression more

--- TrustColorChanger/TrustColoringRank.cs
+++ TrustColorChanger/TrustColoringRank.cs
@@ -55,20 +55,17 @@ namespace TrustColorChanger
 			}
 		}
 
-		public static Color GetVRCPlayer(this TrustColoringRank rank)
+		public static Color GetVRCPlayer(this TrustColoringRank rank) => rank switch
 		{
-			return rank switch
-			{
-				TrustColoringRank.Staff => VRCPlayer.field_Internal_Static_Color_0,
-				TrustColoringRank.Friend => VRCPlayer.field_Internal_Static_Color_1,
-				TrustColoringRank.TrustedUser => VRCPlayer.field_Internal_Static_Color_6,
-				TrustColoringRank.KnownUser => VRCPlayer.field_Internal_Static_Color_5,
-				TrustColoringRank.User => VRCPlayer.field_Internal_Static_Color_4,
-				TrustColoringRank.NewUser => VRCPlayer.field_Internal_Static_Color_3,
-				TrustColoringRank.Visitor => VRCPlayer.field_Internal_Static_Color_2,
-				TrustColoringRank.Nuisance => VRCPlayer.field_Internal_Static_Color_8,
-				_ => throw new System.Exception("Invalid trust rank!"),
-			};
-		}
+			TrustColoringRank.Staff => VRCPlayer.field_Internal_Static_Color_0,
+			TrustColoringRank.Friend => VRCPlayer.field_Internal_Static_Color_1,
+			TrustColoringRank.TrustedUser => VRCPlayer.field_Internal_Static_Color_6,
+			TrustColoringRank.KnownUser => VRCPlayer.field_Internal_Static_Color_5,
+			TrustColoringRank.User => VRCPlayer.field_Internal_Static_Color_4,
+			TrustColoringRank.NewUser => VRCPlayer.field_Internal_Static_Color_3,
+			TrustColoringRank.Visitor => VRCPlayer.field_Internal_Static_Color_2,
+			TrustColoringRank.Nuisance => VRCPlayer.field_Internal_Static_Color_8,
+			_ => throw new System.Exception("Invalid trust rank!"),
+		};
 	}
 }
